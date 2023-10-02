8a3604a110a23be91954c4686ec1fa516bb47779
Hibatullah Dyfa Grahatama <hd.grahatama@gmail.com>
2023-10-02 13:20:30 +0700

    pergantian judul form pendaftaran

--- Modules/PPDB/Resources/views/backend/dataMurid/show.blade.php
+++ Modules/PPDB/Resources/views/backend/dataMurid/show.blade.php
@@ -15,7 +15,7 @@
         <div class="content-header-left col-md-9 col-12 mb-2">
             <div class="row breadcrumbs-top">
                 <div class="col-12">
-                    <h2>Form Pendaftaran PPDB SMK Yadika Natar</h2>
+                    <h2>Form Pendaftaran Savaana Courses</h2>
                 </div>
             </div>
         </div>
