f7bb364a10f1f92ac374c7afe8731c072f0ffdd3
Hibatullah Dyfa Grahatama <hd.grahatama@gmail.com>
2023-10-02 13:44:34 +0700

    penambahan data pada bagian ppdb num

--- Modules/PPDB/Resources/views/frontend/content/count.blade.php
+++ Modules/PPDB/Resources/views/frontend/content/count.blade.php
@@ -9,6 +9,10 @@
                 <h2 class="about-counter title-bar-counter" data-num="10">10</h2>
                 <p>FASILTAS SAVAANA</p>
             </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 counter1-box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".60s">
+                <h2 class="about-counter title-bar-counter" data-num="4">4</h2>
+                <p>PROGRAM</p>
+            </div>
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 counter1-box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".80s">
                 <h2 class="about-counter title-bar-counter" data-num="175">175</h2>
                 <p>MURID TERDAFTAR</p>
