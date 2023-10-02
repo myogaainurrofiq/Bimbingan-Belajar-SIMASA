aecdce954721abdf6884fa8c65da4d6a664f0a5a
Hibatullah Dyfa Grahatama <hd.grahatama@gmail.com>
2023-10-02 13:42:40 +0700

    perubahan data jumlah num di ppdb

--- Modules/PPDB/Resources/views/frontend/content/count.blade.php
+++ Modules/PPDB/Resources/views/frontend/content/count.blade.php
@@ -2,19 +2,15 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 counter1-box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".20s">
-                <h2 class="about-counter title-bar-counter" data-num="20">20</h2>
+                <h2 class="about-counter title-bar-counter" data-num="43">43</h2>
                 <p>PENGAJAR PROFESSIONAL</p>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 counter1-box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".40s">
-                <h2 class="about-counter title-bar-counter" data-num="20">20</h2>
-                <p>FASILTAS SEKOLAH</p>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 counter1-box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".60s">
                 <h2 class="about-counter title-bar-counter" data-num="10">10</h2>
-                <p>EKSTRAKULIKULER</p>
+                <p>FASILTAS SAVAANA</p>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 counter1-box wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".80s">
-                <h2 class="about-counter title-bar-counter" data-num="77">77</h2>
+                <h2 class="about-counter title-bar-counter" data-num="175">175</h2>
                 <p>MURID TERDAFTAR</p>
             </div>
         </div>
